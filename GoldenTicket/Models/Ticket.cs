9a48c2c68375e442321e11172b77a1c8c4ab0761
Damian Adams <DamianAdams96@hotmail.com>
2016-10-03 12:41:04 -0400

    git add *

---
+++ GoldenTicket/Models/Ticket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoldenTicket.Models
+{
+    public class Ticket
+    {
+
+        public 
+    }
+}
--- GoldenTicket/Models/User.cs
+++ GoldenTicket/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,8 +13,13 @@ namespace GoldenTicket.Models
         public string email { get; set; }
         public string password { get; set; }
 
-        public string FirstName { get; set;}
+        [StringLength(50, ErrorMessage = "First Name can not be more than 50 Characters long")]
+        public string FirstName { get; set; }
+
+
         public char MdlInit { get; set; }
+
+        [StringLength(25, ErrorMessage = "Last Name can not be more than 25 Characters long" )]
         public string LastName { get; set; }
 
         public string StreetAddress { get; set; }
@@ -32,6 +38,10 @@ namespace GoldenTicket.Models
 
         public string securityQ3 { get; set; } // Security questions for forgotten password recovery
         public string SecirutyA3 { get; set; } // answer to security question
+
+        public Boolean Employee { get; set; }
+
+        public Boolean Admin { get; set; }
 
     }
 }
